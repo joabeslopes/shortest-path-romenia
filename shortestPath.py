e49690fe1e7772019d37d6267fc93ea9bf1dc2c8
Joabe Lopes <104812737+joabeslopes@users.noreply.github.com>
2024-04-18 09:05:29 -0300

    simplificando cityList

--- shortestPath.py
+++ shortestPath.py
@@ -48,40 +48,32 @@ def shortestPath(graph: dict, sourceNode: str, destNode: str):
 
 
 def createGraph():
-    cityList = [
-        "Oradea", "Zerind", "Arad", "Timisoara", "Lugoj",
-        "Mehadia", "Dobreta", "Sibiu", "Rimnicu Vilcea", "Craiova",
-        "Fagaras", "Pitesti", "Bucharest", "Giurgiu", "Urziceni",
-        "Vaslui", "Iasi", "Neamt", "Hirsova", "Eforie"
-    ]
 
-    neighborsList = [
-        {"Sibiu": 151, "Zerind": 71},
-        {"Oradea": 71, "Arad": 75},
-        {"Zerind": 75, "Sibiu": 140, "Timisoara": 118},
-        {"Arad": 118, "Lugoj": 111},
-        {"Timisoara": 111, "Mehadia": 70},
-        {"Lugoj": 70, "Dobreta": 75},
-        {"Mehadia": 75, "Craiova": 120},
-        {"Arad": 140, "Oradea": 151, "Fagaras": 99, "Rimnicu Vilcea": 80},
-        {"Sibiu": 80, "Craiova": 146, "Pitesti": 97},
-        {"Dobreta": 120, "Rimnicu Vilcea": 146, "Pitesti": 138},
-        {"Sibiu": 99, "Bucharest": 211},
-        {"Rimnicu Vilcea": 97, "Bucharest": 101, "Craiova": 138},
-        {"Fagaras": 211, "Pitesti": 101, "Giurgiu": 90, "Urziceni": 85},
-        {"Bucharest": 90},
-        {"Bucharest": 85, "Hirsova": 98, "Vaslui": 142},
-        {"Urziceni": 142, "Iasi": 92},
-        {"Vaslui": 92, "Neamt": 87},
-        {"Iasi": 87},
-        {"Urziceni": 98, "Eforie": 86},
-        {"Hirsova": 86}
-    ]
+    cityList = {
+        "Oradea": {"Sibiu": 151, "Zerind": 71},
+        "Zerind": {"Oradea": 71, "Arad": 75},
+        "Arad": {"Zerind": 75, "Sibiu": 140, "Timisoara": 118},
+        "Timisoara": {"Arad": 118, "Lugoj": 111},
+        "Lugoj": {"Timisoara": 111, "Mehadia": 70},
+        "Mehadia": {"Lugoj": 70, "Dobreta": 75},
+        "Dobreta": {"Mehadia": 75, "Craiova": 120},
+        "Sibiu": {"Arad": 140, "Oradea": 151, "Fagaras": 99, "Rimnicu Vilcea": 80},
+        "Rimnicu Vilcea": {"Sibiu": 80, "Craiova": 146, "Pitesti": 97},
+        "Craiova": {"Dobreta": 120, "Rimnicu Vilcea": 146, "Pitesti": 138},
+        "Fagaras": {"Sibiu": 99, "Bucharest": 211},
+        "Pitesti": {"Rimnicu Vilcea": 97, "Bucharest": 101, "Craiova": 138},
+        "Bucharest": {"Fagaras": 211, "Pitesti": 101, "Giurgiu": 90, "Urziceni": 85},
+        "Giurgiu": {"Bucharest": 90},
+        "Urziceni": {"Bucharest": 85, "Hirsova": 98, "Vaslui": 142},
+        "Vaslui": {"Urziceni": 142, "Iasi": 92},
+        "Iasi": {"Vaslui": 92, "Neamt": 87},
+        "Neamt": {"Iasi": 87},
+        "Hirsova": {"Urziceni": 98, "Eforie": 86},
+        "Eforie": {"Hirsova": 86}
+    }
 
     graph = {}
-    for c in range(len(cityList)):
-        city = cityList[c]
-        neighbors = neighborsList[c]
+    for city, neighbors in cityList.items():
         graph[city] = Node(city, neighbors)
 
     return graph
